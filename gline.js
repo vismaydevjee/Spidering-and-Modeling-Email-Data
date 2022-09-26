92ceca0ea83239e4b804db58713b06b02c16e77a
Vismay Devjee <devjeevismay@gmail.com>
2022-09-26 11:51:49 +0530

    Add files via upload

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','indiana.edu','mac.com','cam.ac.uk','berkeley.edu','uct.ac.za','ucdavis.edu','gmail.com','ufp.pt','yale.edu'],
+['2005-',57,12,12,7,12,14,11,10,10,5],
+['2006-',456,157,155,152,147,108,109,102,88,90]
+];
